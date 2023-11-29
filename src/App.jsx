009bb5ecec4bb544af059160a9d88e29615c1361
Kauê Evangelista <kauesilva38@hotmail.com>
2023-11-29 11:10:52 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 function App() {
-  return <>
-            <header>
-              <h1>Olá React</h1>
-            </header>
+  return (
+    <>
+      <header>
+        <h1>Olá React</h1>
+        <hr />
+        <nav>
+          <a href="">Home</a>
+          <a href="">Produtos</a>
+          <a href="">Contato</a>
+        </nav>
+      </header>
 
-            {/* Comentario no meio do js é normal ficar entre {} nesta situação */}
-  
-            <main>
-              <h2>Teste</h2>
-            </main>
-          </>
-  
+      {/* Comentario no meio do js é normal ficar entre {} nesta situação */}
 
+      <main>
+        <h2>Teste</h2>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
