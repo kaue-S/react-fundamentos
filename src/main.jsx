5d23c221572216a22ca535e9b756c21458b33dc7
Kauê Evangelista <kauesilva38@hotmail.com>
2023-11-29 10:01:10 -0300

    comit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
+// importações das libs essenciais (react e react DOM)
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
+// Importação do primeiro/principal componente, arquivo em que carregamos os elementos da página (como se fosse um index/home)
+import App from './App.jsx'
+
+
+// inicialização dos recursos do react, manipulando a div #root existewnte no inde.html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
