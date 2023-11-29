8213b3cdff4577b34fa16fce0296e496259554b2
Kauê Evangelista <kauesilva38@hotmail.com>
2023-11-29 11:00:29 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ function App() {
             <header>
               <h1>Olá React</h1>
             </header>
+
+            {/* Comentario no meio do js é normal ficar entre {} nesta situação */}
   
             <main>
               <h2>Teste</h2>
