9e559c2b5da964323a1863fa138b824903cc3a1f
Kauê Evangelista <kauesilva38@hotmail.com>
2023-11-29 10:53:15 -0300

    uso de framgento

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  return <h1>Olá React</h1>
+  return <>
+            <header>
+              <h1>Olá React</h1>
+            </header>
+  
+            <main>
+              <h2>Teste</h2>
+            </main>
+          </>
+  
+
 }
 
 export default App
