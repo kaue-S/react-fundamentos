ad28c9f21bbea1584c224b9d7dbc6f66341e5c64
Kauê Evangelista <kauesilva38@hotmail.com>
2023-11-29 11:56:38 -0300

    separação de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,15 @@
+import Cabecalho from "./components/Cabecalho";
+import Conteudo from "./components/Conteudo";
+import Rodape from "./components/Rodape";
+
 function App() {
   return (
     <>
-      <header>
-        <h1>Olá React</h1>
-        <hr />
-        <nav>
-          <a href="">Home</a>
-          <a href="">Produtos</a>
-          <a href="">Contato</a>
-        </nav>
-      </header>
+      <Cabecalho />
+      <Conteudo />
+      <Rodape />
 
       {/* Comentario no meio do js é normal ficar entre {} nesta situação */}
-
-      <main>
-        <h2>Teste</h2>
-        <section>
-          <h2>Conteúdo da aplicação</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo hic
-            nostrum velit dignissimos odio architecto!
-          </p>
-
-          <article>
-            <h3>Artigo 1</h3>
-            <p>Conteudo do artigo 1.....</p>
-          </article>
-
-          <article>
-            <h3>Artigo 2</h3>
-            <p>Conteudo do artigo 2.....</p>
-          </article>
-
-          <article>
-            <h3>Artigo 3</h3>
-            <p>Conteudo do artigo 3.....</p>
-          </article>
-        </section>
-      </main>
-      <footer>
-        <h2>Rodapé da aplicação</h2>
-      </footer>
     </>
   );
 }
---
+++ src/components/Conteudo.jsx
@@ -0,0 +1,31 @@
+function Conteudo() {
+  return (
+    <main>
+      <h2>Teste</h2>
+      <section>
+        <h2>Conteúdo da aplicação</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo hic
+          nostrum velit dignissimos odio architecto!
+        </p>
+
+        <article>
+          <h3>Artigo 1</h3>
+          <p>Conteudo do artigo 1.....</p>
+        </article>
+
+        <article>
+          <h3>Artigo 2</h3>
+          <p>Conteudo do artigo 2.....</p>
+        </article>
+
+        <article>
+          <h3>Artigo 3</h3>
+          <p>Conteudo do artigo 3.....</p>
+        </article>
+      </section>
+    </main>
+  );
+}
+
+export default Conteudo;
