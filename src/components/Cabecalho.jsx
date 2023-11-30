5c520e2599765f007165c34f20cf662ebf3ef906
Kauê Evangelista <kauesilva38@hotmail.com>
2023-11-30 10:47:41 -0300

    comit

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,9 +1,11 @@
 import Menu from "./Menu";
-
+import "./cabecalho.module.css";
 function Cabecalho() {
   return (
     <header>
-      <h1>Olá React</h1>
+      <h1>
+        Olá <span className="sombra">React</span>
+      </h1>
       <hr />
       <Menu />
     </header>
