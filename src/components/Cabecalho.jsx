703c89394f86d414e5b61b647d1b6f18fe061e2e
Kauê Evangelista <kauesilva38@hotmail.com>
2023-12-01 09:16:13 -0300

    comit

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,12 +1,20 @@
 import Menu from "./Menu";
 
+// importação do styled components
+import styled from "styled-components";
+
+const StyledCabecalho = styled.header`
+  background-color: black;
+  color: white;
+`;
+
 function Cabecalho() {
   return (
-    <header>
+    <StyledCabecalho>
       <h1>Olá React</h1>
       <hr />
       <Menu />
-    </header>
+    </StyledCabecalho>
   );
 }
 
