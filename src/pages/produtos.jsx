5cbb08e8e6edf3ef8501842bf1cfa92a6340df23
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-01-15 12:01:14 -0300

    Update produtos.jsx

--- src/pages/produtos.jsx
+++ src/pages/produtos.jsx
@@ -5,32 +5,57 @@ function Produtos() {
   // o state "produtos" é iniciado com um array vazio. posteriormente (após o carregamento dos dados da api), ele será preenchido com os objetos/produtos
 
   const [produtos, setProdutos] = useState([]);
-    // Gerenciando o efeito colateral do componente para o carregamento dos dados da API
 
+  //state de loading
+  const [loading, setLoading] = useState(true);
 
-    //Fluxo geral do funcionamento do codigo abaixo:
-    // 1- useEffect é carregado UMA vez e após a montagem da page (produtos no caso)
-    // 2- em seguida ele executa a função carregaDados
-    // 3- Ao término dela, atualiza o state(produtos)
+  // Gerenciando o efeito colateral do componente para o carregamento dos dados da API
 
-  useEffect( ()=>{
+  //Fluxo geral do funcionamento do codigo abaixo:
+  // 1- useEffect é carregado UMA vez e após a montagem da page (produtos no caso)
+  // 2- em seguida ele executa a função carregaDados
+  // 3- Ao término dela, atualiza o state(produtos)
 
-      const carregarDados = async () => {
-        try {
-          const resposta = await fetch(`https://fakestoreapi.com/products`);
-          const dados = await resposta.json();
-          console.log(dados);
-          setProdutos(dados);
-        } catch (error) {
-          console.error("houve um erro: " + error);
-        }
-      };
-    
-      carregarDados();
+  useEffect(() => {
+    const carregarDados = async () => {
+      try {
+        const resposta = await fetch(`https://fakestoreapi.com/products`);
+        const dados = await resposta.json();
+        console.log(dados);
+        setProdutos(dados); //adicionamos o state
+        setLoading(false); //desativamos o loading
+      } catch (error) {
+        console.error("houve um erro: " + error);
+      }
+    };
 
-  }, [] );
+    carregarDados();
+  }, []);
 
-  return <h2>Produtos</h2>;
+  return (
+    <article>
+      <h2>Produtos</h2>
+
+      {loading ? (
+        <p>Carregando...</p>
+      ) : (
+        produtos.map((produto) => {
+          return (
+            <section key={produto.id}>
+              <h3>{produto.title}</h3>
+              <b>Preço: {produto.price}</b>
+              <p>
+                <b>descrição:</b> {produto.description}
+              </p>
+              <br />
+            </section>
+          );
+        })
+      )}
+    </article>
+  );
 }
 
 export default Produtos;
+
+//
