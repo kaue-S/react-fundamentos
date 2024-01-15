af98cb191c8a8bbe714be72ad7ae08adcf2d8b5d
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-01-15 10:52:04 -0300

    uso do fetch

--- src/pages/produtos.jsx
+++ src/pages/produtos.jsx
@@ -1,5 +1,19 @@
 function Produtos(){
+
+    const carregarDados = async () => {
+        try {
+            const resposta = await fetch(`https://fakestoreapi.com/products`);
+            const dados = await resposta.json();
+            console.log(dados);
+        } catch (error) {
+            console.error("houve um erro: "+error);
+        }
+    }
+
+    carregarDados();
+    
     return <h2>Produtos</h2>;
+    
 }
 
 export default Produtos;
