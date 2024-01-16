9d8ba3ba4a0628e3caf52dd50bc5167caa2c4c76
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-01-16 09:32:25 -0300

    Update DetalhesProduto.jsx

--- src/pages/DetalhesProduto.jsx
+++ src/pages/DetalhesProduto.jsx
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function DetalhesProduto(){
     const {id} = useParams();
+
+    useEffect( () => {
+        const carregarDados = async () =>{
+            try {
+                const resposta = await fetch(`https://fakestoreapi.com/products/${id}`);
+                const dados = await resposta.json();
+                console.log(dados);
+            } catch (error) {
+                console.log("Erro ao carregar produtos: "+error);
+    
+            }
+        }
+        
+        carregarDados();
+    }, [id]);
+
+
     return <article>
             <h2>Título...</h2>
             <p><b>Categoria: </b>categoria...</p>
